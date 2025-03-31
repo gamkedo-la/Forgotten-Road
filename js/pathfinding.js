0c74ae2312427512571a7143fc22a308e98cdf15
Christer Kaitila <breakdance@mcfunkypants.com>
2025-03-31 09:44:42 -0700

    infinite loop detection and aborting added to pathfinding

--- js/pathfinding.js
+++ js/pathfinding.js
@@ -1,3 +1,7 @@
+// if openset is larger than this, assume pathfinding
+// is stuck in an infinite loop and exit
+const PATHFINDING_TOO_MANY_NODES = 25000; 
+
 // GridElement Class for Pathfinding
 class GridElement {
   constructor(x, y, walkable) {
@@ -13,13 +17,23 @@ class GridElement {
 
 // A* Pathfinding Algorithm
 function findPath(startX, startY, endX, endY, collisionGrid) {
+  console.log("starting pathfinding from "+startX+","+startY+" to "+endX+","+endY);
   let openSet = [];
   let closedSet = [];
   let startNode = new GridElement(startX, startY, true);
   let endNode = new GridElement(endX, endY, true);
+
+  if (!startNode) { console.error("PATHFINDING ERROR: no start node!"); return []; }
+  if (!endNode) { console.error("PATHFINDING ERROR: no end node!"); return []; }
+  if (!collisionGrid) { console.error("PATHFINDING ERROR: no collisionGrid!"); return []; }
+
   openSet.push(startNode);
 
   while (openSet.length > 0) {
+      if (openSet.length>PATHFINDING_TOO_MANY_NODES) { // sanity check to avoid infinite hangs
+          console.log("ERROR: openSet.length="+openSet.length+" is so big we must be stuck in an infinite loop.\npathfinding failed.");
+          return [];
+      }
       let lowestIndex = 0;
       for (let i = 1; i < openSet.length; i++) {
           if (openSet[i].f < openSet[lowestIndex].f) {
@@ -36,6 +50,7 @@ function findPath(startX, startY, endX, endY, collisionGrid) {
               path.push(temp);
               temp = temp.parent;
           }
+          console.log("finished pathfinding! path found ok.");
           return path.reverse();
       }
       
@@ -63,13 +78,14 @@ function findPath(startX, startY, endX, endY, collisionGrid) {
               neighbor.f = neighbor.g + neighbor.h;
               neighbor.parent = currentNode;
           }
-      }
-  }
+      } // loop neighbors
+  } // repeat until openset is empty
   
   //console.log("Path not found from "+startX+","+startY+" to "+endX+","+endY);
   // no such function:
   // let path = AStarAlgorithm(startX, startY, endX, endY, collisionGrid); // A* function
   // return path || []; // Ensure it returns an array
+  console.log("finished pathfinding! path NOT found.");
   return [];
 }
 
